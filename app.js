99302d0307c594292eef2d1fce100575fa416dcf
jmockbee <mockbee1000@gmail.com>
2019-03-08 13:57:29 -0500

    got the dice to work with the rolls and set everything to 0

--- app.js
+++ app.js
@@ -9,24 +9,41 @@ GAME RULES:
 
 */
 
-var scores, roundScore, activePlayer, dice;
+var scores, roundScore, activePlayer;
 
 scores = [0,0];
 roundScore = 0;
 activePlayer = 1;
 
-dice = Math.floor(Math.random() * 6) + 1;
+
+document.querySelector('.dice').style.display = 'none';
+
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
 
 
-document.querySelector('#current-' + activePlayer).textContent = dice;
+
+document.querySelector('.btn-roll').addEventListener('click',function() {
+   // random number
+    dice = Math.floor(Math.random() * 6) + 1;
+    //display 
+    document.querySelector('.dice').style.display = 'block';
+    var diceDom =document.querySelector('.dice');
+    diceDom.style.display = 'block';
+    diceDom.src = 'dice-'+ dice + '.png';
+    // update score 
+});
+
+
+
+
+//document.querySelector('#current-' + activePlayer).textContent = dice;
 //this does the same thing as the code above it
 //document.querySelector ('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
  
 
 
-var x  =document.querySelector('#score-0').textContent;
-console.log (x);
-
-document.querySelector('.dice').style.display = 'none';
-
-
+//var x  =document.querySelector('#score-0').textContent;
+//console.log (x);
